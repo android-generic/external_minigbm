3d5e88869033e167ab605e3da0b8e1c0b5f40e6a
Mauro Rossi <issor.oruam@gmail.com>
2018-11-11 19:37:48 +0100

    android: fix amdgpu DRI_PATH as /vendor/{lib64,lib}/dri
    The correct DRI_PATH is /vendor/{lib64,lib}/dri based on 64,32 bit target
    Linux distributions case is omitted to concentrate on android-x86 builds
    
    Fixes following SIGABRT for 64 bit builds:
    
    E [minigbm:gralloc0.cc(159)] Failed to initialize driver.
    F Gralloc0Allocator: Failed to open gralloc0 device
    F libc    : Fatal signal 6 (SIGABRT), code -6 in tid 2654 (surfaceflinger)
    ...
    F DEBUG   : pid: 2654, tid: 2654, name: surfaceflinger  >>> /system/bin/surfaceflinger <<<
    F DEBUG   : signal 6 (SIGABRT), code -6 (SI_TKILL), fault addr --------
    F DEBUG   : Abort message: 'failed to open gralloc0 device: No such device'

--- amdgpu.c
+++ amdgpu.c
@@ -23,10 +23,10 @@
 #include "drv_priv.h"
 #include "util.h"
 
-#ifdef __ANDROID__
-#define DRI_PATH "/vendor/lib/dri/radeonsi_dri.so"
+#ifdef __LP64__
+#define DRI_PATH "/vendor/lib64/dri/radeonsi_dri.so"
 #else
-#define DRI_PATH "/usr/lib64/dri/radeonsi_dri.so"
+#define DRI_PATH "/vendor/lib/dri/radeonsi_dri.so"
 #endif
 
 #define TILE_TYPE_LINEAR 0
