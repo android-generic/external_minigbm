66825efdaf904ab38cc4620e45f04878bd9a7f67
SupeChicken666 <me@supechicken666.dev>
2026-02-16 02:44:57 +0800

    gbm_mesa: Force usage of linear buffer for hwcodecs
    This fixes HW video playback w/ RGB on ARL iGPUs
    
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- gbm_mesa_driver/gbm_mesa_internals.cpp
+++ gbm_mesa_driver/gbm_mesa_internals.cpp
@@ -376,6 +376,7 @@ int gbm_mesa_bo_create2(struct bo *bo, uint32_t width, uint32_t height, uint32_t
 	/* hwcodecs with c2.ffmpeg's vaapi drm */
 	if (unmask64(&use_flags, BO_USE_HW_VIDEO_DECODER | BO_USE_HW_VIDEO_ENCODER)) {
 		scanout_strong = false;
+		alloc_args.force_linear = true;
 		alloc_args.use_scanout = true;
 		alloc_args.width = ALIGN(alloc_args.width, 32);
 	}
